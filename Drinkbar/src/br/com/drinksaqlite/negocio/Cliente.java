4ffa34696a693eaf20d6de8434ea180da6cd3f63
Diego <Diego@192.168.25.2>
2013-11-14 19:05:47 -0300

    renomeando a class Cliente

---
+++ Drinkbar/src/br/com/drinksaqlite/negocio/Cliente.java
@@ -0,0 +1,109 @@
+package br.com.drinksaqlite.negocio;
+
+public class Cliente {
+
+	private String nome;
+	private String endereco;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	private String cep;
+	private String telefone;
+	private String cpf;
+	private char sexo;
+
+	
+	
+	public Cliente() {
+		super();
+	}
+
+	public Cliente(String nome, String endereco, String bairro, String cidade,
+			String estado, String cep, String telefone, String cpf, char sexo) {
+		super();
+		this.nome = nome;
+		this.endereco = endereco;
+		this.bairro = bairro;
+		this.cidade = cidade;
+		this.estado = estado;
+		this.cep = cep;
+		this.telefone = telefone;
+		this.cpf = cpf;
+		this.sexo = sexo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+	
+	
+
+}
