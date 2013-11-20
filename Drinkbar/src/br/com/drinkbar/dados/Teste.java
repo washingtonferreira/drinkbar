62d0bfee388a3876896c9f2608a962958b77ab0d
Diego <Diego@192.168.25.15>
2013-11-20 17:35:15 -0200

    mainLine

--- Drinkbar/src/br/com/drinkbar/dados/Teste.java
+++ Drinkbar/src/br/com/drinkbar/dados/Teste.java
@@ -1,13 +1,14 @@
 package br.com.drinkbar.dados;
 
-import br.com.drinkbar.gui.JanelaPrincipal;
+import br.com.drinkbar.gui.TelaPrincipal;
 
 public class Teste {
 
 	public static void main(String[] args) {
 
-		JanelaPrincipal janelaPrincipal = new JanelaPrincipal();
-		janelaPrincipal.setVisible(true);
+		TelaPrincipal tp = new TelaPrincipal();
+		tp.setLocationRelativeTo(null);
+		tp.setVisible(true);
 	}
 
 }
