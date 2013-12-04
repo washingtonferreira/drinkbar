8acff1342f291150f06801f677a87454fa9901c2
Diego <Diego@192.168.25.2>
2013-12-04 16:38:18 -0300

    Criando a FrameMainCliente

---
+++ Drinkbar/src/br/com/drinkbar/gui/FrameMaimCliente.java
@@ -0,0 +1,31 @@
+package br.com.drinkbar.gui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTabbedPane;
+
+public class FrameMaimCliente extends JFrame {
+
+	private JPanel contentPane;
+	private PainelCadastroCliente painelCliente;
+	private JTabbedPane tabbedPane;
+
+	public FrameMaimCliente() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 720, 551);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+
+		painelCliente = new PainelCadastroCliente();
+
+		tabbedPane = new JTabbedPane();
+		tabbedPane.addTab("Cadastro", painelCliente);
+		contentPane.add(tabbedPane, BorderLayout.CENTER);
+	}
+
+}
